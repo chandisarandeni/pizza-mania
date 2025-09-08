4ad08cdc6df5792c9887dea802b96de2fc68e26a
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-09-08 21:08:38 +0530

    Add Get Started button to MainActivity
    Replaces the layout in MainActivity with activity_main and adds a Get Started button that navigates to LoginActivity when clicked.

--- app/src/main/java/com/pizzamania/MainActivity.java
+++ app/src/main/java/com/pizzamania/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pizzamania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_checkout_failure);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnGetStarted = findViewById(R.id.btn_get_started);
+        btnGetStarted.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
     }
+
+
 }
