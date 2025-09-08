9e61fbbafe0d82d5348d66d5e592bd515870dce8
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-09-08 21:17:21 +0530

    Add basic login validation and navigation
    Implements simple login logic in LoginActivity. Navigates to PizzaListActivity on correct credentials and shows a toast message on failure.

--- app/src/main/java/com/pizzamania/LoginActivity.java
+++ app/src/main/java/com/pizzamania/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.pizzamania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,24 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        EditText etEmail = findViewById(R.id.et_email);
+        EditText etPassword = findViewById(R.id.et_password);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(v -> {;
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (email.equals("admin") && password.equals("password")) {
+                // Navigate to PizzaListActivity
+                startActivity(new Intent(LoginActivity.this, PizzaListActivity.class));
+                finish(); // Optional: Close the LoginActivity so user can't go back to it
+            } else {
+                Toast.makeText(this, "Enter username and password", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
