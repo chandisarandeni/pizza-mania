f7612299d2731569c62d0a00bf7b4bde3d98dadb
chandisarandeni <chandisarandeni2003@gmail.com>
2025-09-15 21:38:43 +0530

    feat: update payment model ✅

--- app/src/main/java/com/pizzamania/context/order/model/Payment.java
+++ app/src/main/java/com/pizzamania/context/order/model/Payment.java
@@ -5,12 +5,27 @@ public class Payment {
     private String card_last4;
     private boolean authorized;
 
-    public String getMethod() { return method; }
-    public void setMethod(String method) { this.method = method; }
+    public String getMethod() {
+        return method;
+    }
 
-    public String getCard_last4() { return card_last4; }
-    public void setCard_last4(String card_last4) { this.card_last4 = card_last4; }
+    public void setMethod(String method) {
+        this.method = method;
+    }
 
-    public boolean isAuthorized() { return authorized; }
-    public void setAuthorized(boolean authorized) { this.authorized = authorized; }
+    public String getCard_last4() {
+        return card_last4;
+    }
+
+    public void setCard_last4(String card_last4) {
+        this.card_last4 = card_last4;
+    }
+
+    public boolean isAuthorized() {
+        return authorized;
+    }
+
+    public void setAuthorized(boolean authorized) {
+        this.authorized = authorized;
+    }
 }
